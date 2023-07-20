ca6233f0f5858e2fbb04a50e824792467d03996b
Ryan Yin <xiaoyin_c@qq.com>
2023-07-20 20:05:25 +0800

    docs: comments

--- rich-demo/scripts/darwin_set_proxy.py
+++ rich-demo/scripts/darwin_set_proxy.py
@@ -1,5 +1,7 @@
 """
-  set proxy for nix-daemon to speed up downloads
+  Set proxy for nix-daemon to speed up downloads
+  You can safely ignore this file if you don't need it.
+
   https://github.com/NixOS/nix/issues/1472#issuecomment-1532955973
 """
 import os
