dc34abf40c5b5b359b8252d84e479e89e4f37308
Ryan Yin <xiaoyin_c@qq.com>
2023-08-01 15:22:32 +0800

    feat: add m-cli as a reference to other undocumented macOS options

--- minimal/modules/system.nix
+++ minimal/modules/system.nix
@@ -6,6 +6,8 @@
   #
   #  All the configuration options are documented here:
   #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+  #  and see the source code of this project to get more undocumented options:
+  #    https://github.com/rgcr/m-cli
   #
   ###################################################################################
 {
