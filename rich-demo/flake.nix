3c18e1f9f55653bee439dedbdd1e5869252cb41c
Ryan Yin <xiaoyin_c@qq.com>
2024-01-24 19:38:32 +0800

    feat: home-manager - use the release branch

--- rich-demo/flake.nix
+++ rich-demo/flake.nix
@@ -25,7 +25,7 @@
 
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs dependencies.
