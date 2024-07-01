7ad6051d7ef2f37f606853af2321d21b8c0d7a84
lentil32 <lentil32@icloud.com>
2024-07-01 15:11:39 +0900

    Uncomment `time.timeZone` (#11)
    https://github.com/LnL7/nix-darwin/issues/359
    Issue fixed

--- rich-demo/modules/system.nix
+++ rich-demo/modules/system.nix
@@ -170,9 +170,7 @@
   ];
 
   # Set your time zone.
-  # comment this due to the issue:
-  #   https://github.com/LnL7/nix-darwin/issues/359
-  # time.timeZone = "Asia/shanghai";
+  time.timeZone = "Asia/shanghai";
 
   # Fonts
   fonts = {
