0226c111e5f8f9121671519a9fd9052c22ee0598
21CSM <81891917+21CSM@users.noreply.github.com>
2024-07-07 21:35:21 -0400

    Fix issue with zsh program in rich-demo (#13)
    The config bashrcExtra does not exist for zsh - should be initExtra

--- rich-demo/home/shell.nix
+++ rich-demo/home/shell.nix
@@ -2,7 +2,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    bashrcExtra = ''
+    initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
   };
