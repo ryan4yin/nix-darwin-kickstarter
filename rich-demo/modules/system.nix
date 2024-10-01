2e1cd70d8e6e500ca66b5ed3eb0c286e7c65e30a
lentil32 <lentil32@icloud.com>
2024-10-01 16:13:27 +0900

    Failed assertions: system.stateVersion option is not defined in your nix-darwin configuration (#18)
    * Update system.nix
    
    * Update system.nix

--- rich-demo/modules/system.nix
+++ rich-demo/modules/system.nix
@@ -11,8 +11,8 @@
   #
   ###################################################################################
 {
-
   system = {
+    stateVersion = 5;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
     activationScripts.postUserActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
