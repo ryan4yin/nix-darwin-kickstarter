ad5e00a8ab0e81b9e2b03cd8528fa7b87a9048a6
Ryan Yin <xiaoyin_c@qq.com>
2025-07-16 09:42:26 +0800

    chore: upgrade to NixOS 25.05

--- rich-demo/modules/system.nix
+++ rich-demo/modules/system.nix
@@ -180,7 +180,7 @@
       font-awesome
 
       # nerdfonts
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+      # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/data/fonts/nerdfonts/shas.nix
       (nerdfonts.override {
         fonts = [
           # symbols icon only
