a7a2b329e293e0015d8ddde88a9abc6701b2cac6
Ryan Yin <xiaoyin_c@qq.com>
2025-07-17 18:12:35 +0800

    fix: renamed options

--- minimal/modules/system.nix
+++ minimal/modules/system.nix
@@ -11,12 +11,6 @@
 {
   system = {
     stateVersion = 5;
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
 
     defaults = {
       menuExtraClock.Show24Hour = true;  # show 24 hour clock
@@ -27,7 +21,7 @@
   };
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
--- rich-demo/modules/system.nix
+++ rich-demo/modules/system.nix
@@ -12,7 +12,7 @@
   ###################################################################################
 {
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   # Set your time zone.
   time.timeZone = "Asia/shanghai";
